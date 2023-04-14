4571cd7850ee9ce17cfcfa647c31dd10f3c62bb8
Amaury Laridon <amaurylaridon@protonmail.com>
2023-04-14 17:58:11 +0200

    Code Cleaning

--- Projet/Data Analysis/data_analysis.jl
+++ Projet/Data Analysis/data_analysis.jl
@@ -26,98 +26,6 @@ using Plots
 using LaTeXStrings
 using Printf
 sc_interp = pyimport("scipy.interpolate")
-
-######################################################## Data Extraction ###############################################################
-
-### Extraction of the data from the CSV files to a DataFrame object ###
-
-data_var = DataFrame(CSV.File("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Data/OMNI_HRO_1MIN_126357.csv"))
-data_ind = DataFrame(CSV.File("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Data/OMNI2_H0_MRG1HR_126357.csv"))
-data_orb = DataFrame(CSV.File("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Data/WI_OR_PRE_126357.csv"))
-### Definition of the arrays ###
-
-time_var_str = data_var[:, 1]
-time_var = range(1, length(time_var_str))
-flow_speed = data_var[:, 2]
-prot_density = data_var[:, 3]
-temperature = data_var[:, 4]
-println(length(flow_speed))
-
-time_ind_str = data_ind[:, 1]
-time_ind = range(1, length(time_ind_str))
-Kp = data_ind[:, 2]
-Dst = data_ind[:, 3]
-Lyman_α = data_ind[:, 4]
-println(length(Kp))
-
-time_orb_str = data_orb[:, 1]
-time_orb = range(1, length(time_orb_str))
-GSE_Z = data_orb[:, 4]
-println(length(GSE_Z))
-
-##################################################### Extrapolation of the data ########################################################
-
-# We need to extrapolate some of the data since the series doesn't have the same length due to differences 
-# in temporal resolation of the instruments.
-
-new_grid = time_ind
-
-interp_func_flow_speed = sc_interp.interp1d(time_var, flow_speed)
-interp_flow_speed = interp_func_flow_speed(new_grid)
-
-interp_func_prot_density = sc_interp.interp1d(time_var, prot_density)
-interp_prot_density = interp_func_prot_density(new_grid)
-
-interp_func_temp = sc_interp.interp1d(time_var, temperature)
-interp_temperature = interp_func_temp(new_grid)
-
-interp_func_GSE = sc_interp.interp1d(time_orb, GSE_Z)
-interp_GSE_Z = interp_func_GSE(new_grid)
-
-#println(length(interp_flow_speed))
-#println(length(interp_prot_density))
-#println(length(interp_temperature))
-#println(length(interp_GSE_Z))
-############################################################ Display ##################################################################
-"""
-data_plot = scatter(interp_Kp, flow_speed)
-title!("Plasma Flow Speed - Kp index relation")
-#plot!(new_grid, interp_Kp, label="Interp Kp")
-#plot!(time_mod, regr_lin_opt, label ="Wieghted least-squares fit ")
-#display(data_plot)
-savefig("flow_speed(Kp).png")
-"""
-
-plot_temperature = plot(time_var, temperature, linewidth=0.2)
-title!("Temperature")
-#plot!(new_grid, interp_Kp, label="Interp Kp")#######################################################################################################################################
-################################################### Wind Mission Data Analysis ########################################################
-# Author: Amaury Laridon 
-# Course: LPHYS2266 - Physics of the Upper Atmosphere and Space 
-# Goal: Extraction of some data from the Wind NASA mission and analysis. 
-#       The goal is to see wether the observed time variations in several variables are due to change
-#       in the orbit of the satellite or to solar activity.
-# Data: Wind data from NASA available at https://cdaweb.gsfc.nasa.gov/index.html 
-# Date: 12/04/23
-#######################################################################################################################################
-#######################################################################################################################################
-
-######################################################## Librairies Loading ###########################################################
-
-using Pkg
-using Interpolations
-using CSV
-using DataFrames
-using Plots
-using DelimitedFiles
-using LaTeXStrings
-using LinearAlgebra
-using Statistics
-using PyCall
-using Plots
-using LaTeXStrings
-using Printf
-sc_interp = pyimport("scipy.interpolate")
 np = pyimport("numpy")
 
 ######################################################## Data Extraction ###############################################################
@@ -149,7 +57,8 @@ GSE_Z = data_orb[:, 4]
 #println(length(GSE_Z))
 
 ### Cleaning of some data ###
-# Some data have unphysical values we have to delete them or bound them. 
+# Some data have unphysical values. To proceed I compute the arithmetic mean from the two previous values
+# before the singularity  
 
 for i in range(1, length(prot_density))
     if prot_density[i] == 999.990
@@ -184,16 +93,9 @@ end
 
 new_var_index = np.linspace(1, length(flow_speed), length(Kp))
 new_var_index = Int.(np.round(new_var_index))
-#new_var_index = Float64.(new_var_index)
-#println(length(new_var_index))
-#println(new_var_index)
 
 new_orb_index = np.linspace(1, length(GSE_Z), length(Kp))
 new_orb_index = Int.(np.round(new_orb_index))
-#new_var_index = Float64.(new_orb_index)
-#println(length(new_orb_index))
-#println(new_orb_index)
-
 
 ##################################################### Extrapolation of the data ########################################################
 
@@ -234,8 +136,6 @@ for i in range(1, length(interp_GSE_Z))
     interp_GSE_Z[i] = GSE_Z[index]
 end
 
-
-
 """
 ### Version with the interp1d function ###
 # We need to extrapolate some of the data since the series doesn't have the same length due to differences 
@@ -263,58 +163,60 @@ interp_GSE_Z = interp_func_GSE(new_grid)
 
 ############################################################ Display ##################################################################
 
+### Display parameters ###
+save_dir = "/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/"
 ##### Individual plot of initial data set and filterd data set #####
-"""
+
 plot_flow_speed = plot(time_var, flow_speed, linewidth=0.5, size=(1400, 900), title="Plasma Flow Speed\nWind Mission Data from 12/02/1997 to 04/29/1998", xlabel="Time [minute]", ylabel="flow speed GSE [km/s]", label="Flow Speed", thickness_scaling=1.5)
 #title!("Plasma Flow Speed\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_flow_speed)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/flow_speed.png")
+savefig(save_dir * "flow_speed.png")
 
 plot_interp_flow_speed = plot(interp_time_grid, interp_flow_speed, linewidth=0.5, size=(1400, 900), xlabel="Time [minute]", ylabel="flow speed GSE [km/s]", label="Flow Speed", thickness_scaling=1.5)
 title!("Interpolated Plasma Flow Speed\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_interp_flow_speed)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/interp_flow_speed.png")
+savefig(save_dir * "interp_flow_speed.png")
 
 plot_prot_density = plot(time_var, prot_density, linewidth=0.5, size=(1400, 900), xlabel="Time [minute]", ylabel="proton density [n/cc]", label="Proton Density", thickness_scaling=1.5)
 title!("Proton Density\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_prot_density)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/prot_density.png")
+savefig(save_dir * "prot_density.png")
 
 plot_interp_prot_density = plot(interp_time_grid, interp_prot_density, linewidth=0.5, size=(1400, 900), xlabel="Time [minute]", ylabel="proton density [n/cc]", label="Proton Density", thickness_scaling=1.5)
 title!("Interpolated Proton Density\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_interp_prot_density)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/interp_prot_density.png")
+savefig(save_dir * "interp_prot_density.png")
 
 plot_temperature = plot(time_var, temperature, linewidth=0.5, size=(1400, 900), xlabel="Time [minute]", ylabel="temperature [k]", label="Temperature", thickness_scaling=1.5)
 title!("Temperature\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_temperature)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/temperature.png")
+savefig(save_dir * "temperature.png")
 
 plot_interp_temperature = plot(interp_time_grid, interp_temperature, linewidth=0.5, size=(1400, 900), xlabel="Time [minute]", ylabel="temperature [k]", label="Temperature", thickness_scaling=1.5)
 title!("Interpolated Temperature\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_interp_temperature)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/interp_temperature.png")
+savefig(save_dir * "interp_temperature.png")
 
 plot_Kp = plot(time_ind, Kp, linewidth=1, size=(1400, 900), xlabel="Time [hour]", ylabel="3-h Kp*10", label="Kp", thickness_scaling=1.5)
 title!("Kp Index\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_Kp)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/Kp.png")
+savefig(save_dir * "Kp.png")
 
 plot_Dst = plot(time_ind, Dst, linewidth=1, size=(1400, 900), xlabel="Time [hour]", ylabel="1-h Dst", label="Dst", thickness_scaling=1.5)
 title!("Dst Index\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_Dst)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/Dst.png")
+savefig(save_dir * "Dst.png")
 
 plot_GSE_Z = plot(time_orb, GSE_Z, linewidth=1, size=(1400, 900), xlabel="Time [10 minutes]", ylabel="Z_GSE", label="GSE_Z", thickness_scaling=1.5)
 title!("GSE_Z\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_GSE_Z)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/GSE_Z.png")
+savefig(save_dir * "GSE_Z.png")
 
 plot_interp_GSE_Z = plot(interp_time_grid, interp_GSE_Z, linewidth=1, size=(1400, 900), xlabel="Time [10 minutes]", ylabel="Z_GSE", label="GSE_Z", thickness_scaling=1.5)
 title!("Interpolated GSE_Z\nWind Mission Data from 12/02/1997 to 04/29/1998")
 #display(plot_interp_GSE_Z)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/interp_GSE_Z.png")
-"""
+savefig(save_dir * "interp_GSE_Z.png")
+
 ################################################## Correlation coefficient ###############################################################
 
 println("-------------------------------------------------------------------------")
@@ -355,6 +257,8 @@ println("Correlation coefficient (Proton density, GSE_Z) = ", cor_prot_dens_GSE)
 println("-------------------------------------------------------------------------")
 
 ##### Correlation Plot #####
+
+# Small random variations in order to distinguish more easily the scatter plot 
 rand_ar = fill(0.0, length(Kp))
 for i in range(1, length(rand_ar))
     rand_ar[i] = 0.3 * randn()
@@ -368,7 +272,7 @@ b = mean(interp_temperature) - a * mean(Kp)
 lr1 = [a * x + b for x in Kp]
 plot!(Kp, lr1, label="Linear Regression")
 #display(plot_Dst)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/cor_temp_Kp_lr.png")
+savefig(save_dir * "cor_temp_Kp_lr.png")
 
 
 plot_cor_flow_speed_Kp = scatter(Kp + rand_ar, interp_flow_speed + rand_ar, linewidth=1, size=(1400, 900), xlabel="3-h Kp*10", ylabel="Plasma Flow Speed [km/s]", label="Kp-Flow Speed", thickness_scaling=1.5)
@@ -378,7 +282,7 @@ b = mean(interp_flow_speed) - a * mean(Kp)
 lr2 = [(a * x) + b for x in Kp]
 plot!(Kp, lr2, label="Linear Regression")
 #display(plot_Dst)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/cor_flow_speed_Kp_lr.png")
+savefig(save_dir * "cor_flow_speed_Kp_lr.png")
 
 plot_cor_flow_speed_Dst = scatter(Dst + rand_ar, interp_flow_speed, linewidth=1, size=(1400, 900), xlabel="1-h Dst nT", ylabel="Plasma Flow Speed [km/s]", label="Dst-Flow Speed", thickness_scaling=1.5)
 title!("Correlation between Dst Index and Plasma Flow Speed\nWind Mission Data from 12/02/1997 to 04/29/1998")
@@ -388,7 +292,7 @@ b = mean(interp_flow_speed) - a * mean(Dst)
 lr3 = [(a * x) + b for x in Dst]
 plot!(Dst, lr3, label="Linear Regression")
 #display(plot_Dst)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/cor_flow_speed_Dst_lr.png")
+savefig(save_dir * "cor_flow_speed_Dst_lr.png")
 
 plot_cor_flow_speed_GSE = scatter(interp_GSE_Z + rand_ar, interp_flow_speed, linewidth=1, size=(1400, 900), xlabel="GSE_Z", ylabel="Plasma Flow Speed [km/s]", label="GSE_Z-Flow Speed", thickness_scaling=1.5)
 title!("Correlation between GSE_Z and Plasma Flow Speed\nWind Mission Data from 12/02/1997 to 04/29/1998")
@@ -398,4 +302,4 @@ b = mean(interp_flow_speed) - a * mean(interp_GSE_Z)
 lr4 = [a * x + b for x in interp_GSE_Z]
 plot!(interp_GSE_Z, lr4, label="Linear Regression")
 #display(plot_Dst)
-savefig("/home/amaury/Bureau/LPHYS2266 - Physics of the upper atmosphere and space/Projet/Data Analysis/Figures/cor_flow_speed_GSE_lr.png")
+savefig(save_dir * "cor_flow_speed_GSE_lr.png")
